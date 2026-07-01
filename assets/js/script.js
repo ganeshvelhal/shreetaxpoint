46f559a54c2a531938be8f9932d842425024f258
ganeshvelhal <ganesh4343@gmail.com>
2026-07-01 20:11:32 +0530

    updatee

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbypf-rUTZheSHcT_X89IpT2PTzqO6RH_lEd6iyzYYf8CccdmGTXEq30-G-IJ_gpatw9BA/exec";
+const scriptURL = "https://https://script.google.com/macros/s/AKfycbypf-rUTZheSHcT_X89IpT2PTzqO6RH_lEd6iyzYYf8CccdmGTXEq30-G-IJ_gpatw9BA/exec";
+
+console.log("script.js loaded");
 
 const form = document.getElementById("contactForm");
 
+console.log(form);
+
 form.addEventListener("submit", function(e) {
+
+    console.log("Form submitted");
+
     e.preventDefault();
 
     fetch(scriptURL, {
         method: "POST",
         body: new FormData(form)
     })
-    .then(() => {
-        alert("✅ Thank you! Your enquiry has been submitted successfully.");
-        form.reset();
+    .then(res => res.text())
+    .then(data => {
+        console.log(data);
+        alert("Success");
     })
-    .catch((error) => {
-        alert("❌ Something went wrong. Please try again.");
-        console.error(error);
+    .catch(err => {
+        console.error(err);
+        alert("Failed");
     });
+
 });
